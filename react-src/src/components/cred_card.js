2f62d5c5c8be9598555541e5ecb6ccd38379ef66
Andreas Krimbacher <a.krimbacher@zamg.ac.at>
2021-04-16 17:50:26 +0200

    fixed opactity

--- react-src/src/components/cred_card.js
+++ react-src/src/components/cred_card.js
@@ -29,8 +29,8 @@ class Cred extends Component {
   render()
   {
     return(
-      <div className='flex h-screen w-full h-full fixed block top-0 left-0 bg-gray-400 opacity-75 z-50'>
-        <div className='relative m-auto rounded-xl p-2 bg-gray-600 opacity-100 z-101'>
+      <div className='flex h-screen w-full h-full fixed block top-0 left-0 bg-gray-400 bg-opacity-75 z-50'>
+        <div className='relative m-auto rounded-xl p-2 bg-gray-600 z-101'>
           <div className='flex item-right'>
             <button className='rounded-full hover:text-red-400 hover:bg-black' onClick={e => this.props.Menu(e)}>x</button>
           </div>
